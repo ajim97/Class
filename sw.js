e2cdda37c72b77293d96d475199b1dac7e8f6745
ajim97 <168499944+ajim97@users.noreply.github.com>
2025-11-27 18:18:48 -0800

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("push", (event) => {
-  const data = event.data.json();
-  self.registration.showNotification(data.title, {
-    body: data.message,
-    icon: "https://cdn-icons-png.flaticon.com/512/1827/1827310.png"
-  });
-});
